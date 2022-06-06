eeee52d50a009c80f0f01012d13eead34acbbbe9
Sergey Matkin <mserg-90@mail.ru>
2022-06-06 21:56:15 +0300

    first commit

---
+++ src/main/java/com/cybertester/cyberTester/CyberTesterApplication.java
@@ -0,0 +1,13 @@
+package com.cybertester.cyberTester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybertester/cyberTester/CyberTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertester.cyberTester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
